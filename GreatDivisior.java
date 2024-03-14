7a97744056ff31c80a2e2e865280cc01b4c9dc5f
piyushkumarsri <piyush100489@gmail.com>
2024-03-14 17:55:18 +0530

    Update GreatDivisior.java

--- GreatDivisior.java
+++ GreatDivisior.java
@@ -3,7 +3,7 @@ package com.advance.com.work.around;
 public class GreatDivisior {
 
 	public static void main(String[] args) {
-		int num=81,num2=153;
+		int num=81,num2=154;
 		int gcd=1;
 		for(int i=1;i<=num-1;i++) {
 			if(num%i==0) {
