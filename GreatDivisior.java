1112c519cbc960938fc5f8b4dc21dc6f7f1a3934
saurabh <piyush100489@gmail.com>
2024-03-14 17:53:50 +0530

    commit5

---
+++ GreatDivisior.java
@@ -0,0 +1,26 @@
+package com.advance.com.work.around;
+
+public class GreatDivisior {
+
+	public static void main(String[] args) {
+		int num=81,num2=153;
+		int gcd=1;
+		for(int i=1;i<=num-1;i++) {
+			if(num%i==0) {
+				gcd=i;
+				
+			}
+			
+		}
+		System.out.println(gcd+" "+num);
+		for(int i=1;i<=num2-1;i++) {
+			if(num2%i==0) {
+				gcd=i;
+				
+			}
+			
+		}
+		System.out.println(gcd+" "+num2);
+	}
+
+}
